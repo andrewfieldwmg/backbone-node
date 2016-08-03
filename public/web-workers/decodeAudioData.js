09123c9ef6baabb9d395af92cde3f81519afc2a8
Andrew Field <andyfield83@gmail.com>
2016-08-03 13:29:33 +0100

    as far as I can, really

--- public/web-workers/decodeAudioData.js
+++
@@ -1,6 +0,0 @@
-onmessage = function(e) {
-  console.log('Message received from main script');
-  var workerResult = 'Result: ' + e.data;
-  console.log('Posting message back to main script');
-  postMessage(e.data);
-}
